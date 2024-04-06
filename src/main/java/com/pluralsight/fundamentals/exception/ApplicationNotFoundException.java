1a878af4515ad680852eaffeaf421c64434528e7
Palash Sarkar <palash.sarkar@protonmail.com>
2024-04-06 23:47:40 +0300

    Define resolver and mutator for graphQL

--- src/main/java/com/pluralsight/fundamentals/exception/ApplicationNotFoundException.java
+++ src/main/java/com/pluralsight/fundamentals/exception/ApplicationNotFoundException.java
@@ -1,8 +1,34 @@
 package com.pluralsight.fundamentals.exception;
 
-public class ApplicationNotFoundException  extends RuntimeException{
+import graphql.ErrorClassification;
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ApplicationNotFoundException  extends RuntimeException implements GraphQLError {
+
+    private Map<String, Object> extensions = new HashMap<>();
 
     public ApplicationNotFoundException(String message) {
         super(message);
+    }
+
+    public ApplicationNotFoundException(String message, Long id) {
+        super(message);
+        extensions.put("invalideApplicationId", id);
+    }
+
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public ErrorClassification getErrorType() {
+        return ErrorType.DataFetchingException;
     }
 }
---
+++ src/main/java/com/pluralsight/fundamentals/resolver/Query.java
@@ -0,0 +1,24 @@
+package com.pluralsight.fundamentals.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.pluralsight.fundamentals.entity.Application;
+import com.pluralsight.fundamentals.repository.ApplicationRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Query  implements GraphQLQueryResolver {
+
+    private final ApplicationRepository applicationRepository;
+
+    public Query(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
+
+    public Iterable<Application> findAllApplications() {
+        return applicationRepository.findAll();
+    }
+
+    public long countApplications() {
+        return applicationRepository.count();
+    }
+}
